6635823bea04157c311f046b13f34ff8b4733e75
Ivan0195 <iihnatsenka95@gmail.com>
2022-02-13 18:53:57 +0300

    lesson4done

---
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import HW4 from "../../h4/HW4";
+import './../style.css'
+
+
+function Junior() {
+    return (
+        <div className='size'>
+            <div className='title'>Наполнение для Junior</div>
+            <HW4/>
+        </div>
+    );
+}
+
+export default Junior;
+
+// сделайте по аналогии пустые страницы джун и джун+
+// туда будут добавляться следующие дз
---
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import HW4 from "../../h4/HW4";
+import './../style.css'
+
+
+function JuniorPlus() {
+    return (
+        <div className='size'>
+            <div className='title'>Наполнение для JuniorPlus</div>
+            <HW4/>
+        </div>
+    );
+}
+
+export default JuniorPlus;
+
+// сделайте по аналогии пустые страницы джун и джун+
+// туда будут добавляться следующие дз
